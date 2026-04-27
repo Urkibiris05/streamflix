87646bcd472fb8d3c5e61eff7ce007dbc7345033
AsierBarrio <asierbarrioborge10@gmail.com>
2026-04-27 12:15:31 +0200

    Cambios para cargar las series de la bd

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def _load_env_file(env_path):
                 os.environ[key] = value
 
 
-_load_env_file(os.path.join(basedir, '.env.example'))
+_load_env_file(os.path.join(basedir, '.env'))
 app = Flask(__name__, static_url_path='', static_folder='.')
 app.config['SECRET_KEY'] = 'tu_clave_secreta'
 
